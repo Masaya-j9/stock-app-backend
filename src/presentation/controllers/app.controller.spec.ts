45c2e1e7b1eaa1d0062f1bd4e218bccd4ae3706b
Masaya-j9 <masaya.spectre@gmail.com>
2024-10-14 22:56:30 +0900

    add health

--- src/presentation/controllers/app.controller.spec.ts
+++ src/presentation/controllers/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from '../../app.service';
 import { lastValueFrom } from 'rxjs';
 
 describe('AppController', () => {
