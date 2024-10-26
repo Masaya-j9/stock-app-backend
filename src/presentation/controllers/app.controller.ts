f228fe30d82a2ce6eda01abb73eb6eb32f969f7f
Masaya-j9 <masaya.spectre@gmail.com>
2024-10-27 00:17:57 +0900

    add api documents

--- src/presentation/controllers/app.controller.ts
+++ src/presentation/controllers/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../../app.service';
 import { Observable } from 'rxjs';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('stock-app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: '起動時のエンドポイント' })
+  @ApiResponse({ status: 200, description: 'デフォルトなので特になし' })
   getHello(): Observable<string> {
     return this.appService.getObservableHelloWorld();
   }
