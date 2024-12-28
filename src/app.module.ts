8f38c429f7dd08640b9afa3a413d31d745a2bb48
Masaya-j9 <masaya.spectre@gmail.com>
2024-12-28 19:04:06 +0900

    add database settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,15 @@ import { ormConfig } from './infrastructure/orm/orm.config';
 import { HealthModule } from './presentation/modules/health.module';
 import { UserModule } from './presentation/modules/user.module';
 import { LoggerInterceptor } from './common/utils/logger.inspector';
+import { DatabaseModule } from './presentation/modules/database.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), HealthModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    HealthModule,
+    UserModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
