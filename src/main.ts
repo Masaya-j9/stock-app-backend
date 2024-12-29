89d219a47f2e621ebd3e28e21c363fa6c281c6a0
Masaya-j9 <masaya.spectre@gmail.com>
2024-12-30 01:41:12 +0900

    fix environment valiadate

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.API_PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
