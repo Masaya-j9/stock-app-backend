549827a9ce59d293616f835eb5c04f872d142612
Masaya-j9 <masaya.spectre@gmail.com>
2025-01-02 14:21:23 +0900

    add SnakeNamingStrategy settings

--- src/infrastructure/orm/orm.config.ts
+++ src/infrastructure/orm/orm.config.ts
@@ -1,12 +1,19 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { ConfigModule } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 ConfigModule.forRoot({
   isGlobal: true,
 });
 
 const configService = new ConfigService();
+
+console.log(configService.get<string>('DB_HOST'));
+console.log(configService.get<number>('DB_PORT'));
+console.log(configService.get<string>('DB_USERNAME'));
+console.log(configService.get<string>('DB_PASSWORD'));
+console.log(configService.get<string>('DB_NAME'));
 
 export const ormConfig: DataSourceOptions = {
   type: 'mysql',
@@ -16,8 +23,14 @@ export const ormConfig: DataSourceOptions = {
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_NAME'),
   entities: ['dist/infrastructure/orm/entities/**/*.js'],
-  synchronize: false, // 開発環境用、本番環境ではfalseに設定
+  namingStrategy: new SnakeNamingStrategy(),
+  synchronize: true, // 開発環境用、本番環境ではfalseに設定
   migrations: ['dist/infrastructure/migrations/**/*.js'],
+  extra: {
+    // Public Key Retrievalを許可
+    connectionLimit: 10,
+    allowPublicKeyRetrieval: true,
+  },
 };
 
 export const AppDataSource = new DataSource(ormConfig);
