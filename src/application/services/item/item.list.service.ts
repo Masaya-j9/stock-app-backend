4110edbcec56fca54ed4a90e1e439204720fb517
Masaya-j9 <masaya.spectre@gmail.com>
2025-02-08 01:01:39 +0900

    add application service

--- src/application/services/item/item.list.service.ts
+++ src/application/services/item/item.list.service.ts
@@ -1,13 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { ItemListServiceInterface } from './item.list.interface';
-import { Observable } from 'rxjs';
-import { ItemListInputDto } from 'src/application/dto/input/item/item.list.input.dto';
-import { ItemListOutputDto } from 'src/application/dto/output/item/item.list.output.dto';
+import { map, Observable, switchMap, of, forkJoin } from 'rxjs';
+import { ItemListInputDto } from '../../dto/input/item/item.list.input.dto';
+import { ItemListOutputDto } from '../../dto/output/item/item.list.output.dto';
+import { ItemListDatasource } from '../../../infrastructure/datasources/items/item.list.datasource';
+import { CategoriesDatasource } from '../../../infrastructure/datasources/categories/categories.datasource';
+import { ItemListOutputBuilder } from '../../dto/output/item/item.list.output.builder';
 
 @Injectable()
 export class ItemListService implements ItemListServiceInterface {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // 登録されている物品情報に関するコンストラクタ
+  constructor(
+    private readonly itemListDatasource: ItemListDatasource,
+    private readonly categoriesDatasource: CategoriesDatasource
+  ) {}
+
+  /**
+   * 登録されいている物品の一覧を取得する
+   * @param {ItemListInputDto} input - リクエスト情報
+   * @returns{Observable<Builder>} - 登録されている物品の一覧情報を含むObservableオブジェクト
+   *
+   * @throws {HttpException} 物品が見つからない場合、404エラーをスローします。
+   */
   service(input: ItemListInputDto): Observable<ItemListOutputDto> {
-    return null;
+    const pageNumber: number = input.pages;
+    const sortOrderNumber: number = input.sortOrder;
+    console.log('Received input:', input);
+    console.log(
+      'Executing findItemList with pageNumber:',
+      pageNumber,
+      'sortOrderNumber:',
+      sortOrderNumber
+    );
+    return this.itemListDatasource
+      .findItemList(pageNumber, sortOrderNumber)
+      .pipe(
+        switchMap((items) => {
+          console.log('Items found:', items);
+          return forkJoin([
+            of(items),
+            this.itemListDatasource.getTotalCount(),
+            this.categoriesDatasource.findByCategories(
+              items.map((item) => item.id)
+            ),
+          ]).pipe(
+            map(([items, totalCount, categories]) => {
+              console.log('Total count:', totalCount);
+              console.log('Categories found:', categories);
+              const builder = new ItemListOutputBuilder();
+              builder.items = items;
+              builder.totalCount = totalCount.count;
+              builder.categories = categories;
+              return builder.build();
+            })
+          );
+        })
+      );
   }
 }
