432da61673c33794a7715ae86b2b917851a8606d
Masaya-j9 <masaya.spectre@gmail.com>
2025-02-08 00:59:33 +0900

    add input dto

--- src/application/dto/input/item/item.list.input.dto.ts
+++ src/application/dto/input/item/item.list.input.dto.ts
@@ -1,2 +1,38 @@
 import { InputDto } from '../input.dto';
-export class ItemListInputDto implements InputDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
+import { Expose, Transform } from 'class-transformer';
+
+/*
+ * 物品一覧を取得する際のリクエスト情報を表すDTO
+ */
+
+// const SORT_ORDER_MIN: number = 0;
+// const SORT_ORDER_MAX: number = 1;
+
+export class ItemListInputDto implements InputDto {
+  @ApiProperty({
+    example: 1,
+    description: `
+    ページ番号
+    `,
+    type: Number,
+  })
+  @IsOptional()
+  @Expose({ name: 'pages' })
+  pages?: number;
+
+  @ApiProperty({
+    example: 0,
+    description: `
+    並べ替え順
+    0: 昇順(省略された場合はデフォルトとする)
+    1: 降順
+    `,
+    type: Number,
+  })
+  @IsOptional()
+  @Transform(({ value }) => Number(value))
+  @Expose({ name: 'sort_order' })
+  sortOrder?: number;
+}
