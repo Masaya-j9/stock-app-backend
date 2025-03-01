956812df5761810df04f0b87b306823d72a1770d
Masaya-j9 <masaya.spectre@gmail.com>
2025-03-02 01:18:29 +0900

    add provider about register

--- src/presentation/modules/categories.module.ts
+++ src/presentation/modules/categories.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryListController } from '../controllers/category/category.controller';
 import { CategoryListService } from '../../application/services/category/category.list.service';
+import { CategoryRegisterService } from '../../application/services/category/category.register.service';
 import { DatabaseModule } from './database.module';
 import { CategoriesDatasource } from '../../infrastructure/datasources/categories/categories.datasource';
 
@@ -14,7 +15,15 @@ import { CategoriesDatasource } from '../../infrastructure/datasources/categorie
       provide: 'CategoryListServiceInterface',
       useClass: CategoryListService,
     },
+    {
+      provide: 'CategoryRegisterServiceInterface',
+      useClass: CategoryRegisterService,
+    },
   ],
-  exports: [CategoriesDatasource, 'CategoryListServiceInterface'],
+  exports: [
+    CategoriesDatasource,
+    'CategoryListServiceInterface',
+    'CategoryRegisterServiceInterface',
+  ],
 })
 export class CategoriesModule {}
