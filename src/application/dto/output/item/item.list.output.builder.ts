edadb1e18ae7f875b8a5abb72cf300857c2a0f1a
Masaya-j9 <masaya.spectre@gmail.com>
2025-03-23 13:06:45 +0900

    change to move mapping responsibilities

--- src/application/dto/output/item/item.list.output.builder.ts
+++ src/application/dto/output/item/item.list.output.builder.ts
@@ -1,8 +1,5 @@
 import { OutputBuilder } from '../../output/output.builder';
-import { ItemListOutputDto } from './item.list.output.dto';
-import { Items } from '../../../../infrastructure/orm/entities/items.entity';
-import { Categories } from '../../../../infrastructure/orm/entities/categories.entity';
-import { ItemAndCategoryType } from '../../../../infrastructure/types/item.and.category.type';
+import { ItemListOutputDto, itemCategories } from './item.list.output.dto';
 import { Item } from '../../../../domain/inventory/items/entities/item.entity';
 import { Category } from '../../../../domain/inventory/items/entities/category.entity';
 import { NotFoundException } from '@nestjs/common';
@@ -11,57 +8,11 @@ export class ItemListOutputBuilder implements OutputBuilder<ItemListOutputDto> {
   private _totalCount: number;
   private _items: Item[];
   private _categories: Category[];
-  private _itemIdsAndCategoryIds: ItemAndCategoryType[];
 
-  constructor(
-    items?: Items[],
-    totalCount?: number,
-    categories?: Categories[],
-    itemIdsAndCategoryIds?: ItemAndCategoryType[]
-  ) {
-    this._totalCount = totalCount ?? 0;
-    this._itemIdsAndCategoryIds = itemIdsAndCategoryIds ?? [];
-    this._items = items
-      ? this.mapItems(items, this._itemIdsAndCategoryIds)
-      : [];
-    this._categories = categories ? this.mapCategories(categories) : [];
-  }
-
-  // CategoriesをCategoryドメインエンティティにマッピング
-  private mapCategories(categories: Categories[]): Category[] {
-    return categories.map((category) => {
-      return new Category(
-        category.id,
-        category.name,
-        category.description,
-        category.createdAt,
-        category.updatedAt,
-        category.deletedAt
-      );
-    });
-  }
-
-  // ItemsをItemドメインエンティティにマッピング
-  private mapItems(
-    items: Items[],
-    itemIdsAndCategoryIds: ItemAndCategoryType[]
-  ): Item[] {
-    return items.map((item) => {
-      const categoryIds = itemIdsAndCategoryIds
-        .filter((itemCategory) => itemCategory.itemId === item.id)
-        .map((itemCategory) => itemCategory.categoryId); // 修正：categoryIdを抽出
-
-      return new Item(
-        item.id,
-        item.name,
-        item.quantity,
-        item.description,
-        item.createdAt,
-        item.updatedAt,
-        item.deletedAt,
-        categoryIds // 修正：取得したcategoryIdsをItemに渡す
-      );
-    });
+  constructor(items: Item[], totalCount: number, categories: Category[]) {
+    this._totalCount = totalCount;
+    this._items = items;
+    this._categories = categories;
   }
 
   build(): ItemListOutputDto {
@@ -92,7 +43,7 @@ export class ItemListOutputBuilder implements OutputBuilder<ItemListOutputDto> {
                     }
                   : null;
               })
-              .filter(Boolean),
+              .filter(Boolean) as itemCategories[],
           }));
           return output;
         })();
