5f4b2fa10d02cf4edd92d9aec275215b531f7da8
Masaya-j9 <masaya.spectre@gmail.com>
2025-04-07 02:05:24 +0900

    add register method to item module

--- src/presentation/modules/items.module.ts
+++ src/presentation/modules/items.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemController } from '../controllers/item/item.controller';
 import { ItemListService } from '../../application/services/item/item.list.service';
+import { ItemRegisterService } from '../../application/services/item/item.register.service';
 import { DatabaseModule } from './database.module';
 import { ItemsDatasource } from 'src/infrastructure/datasources/items/items.datasource';
 import { CategoriesModule } from './categories.module';
@@ -14,6 +15,10 @@ import { CategoriesModule } from './categories.module';
     {
       provide: 'ItemListServiceInterface',
       useClass: ItemListService,
+    },
+    {
+      provide: 'ItemRegisterServiceInterface',
+      useClass: ItemRegisterService,
     },
   ],
 })
