79e7b307911c140297f24010e92d14432aa3ab88
Masaya-j9 <masaya.spectre@gmail.com>
2025-04-29 11:59:14 +0900

    add update-item endpoint

--- src/presentation/controllers/item/item.controller.ts
+++ src/presentation/controllers/item/item.controller.ts
@@ -1,12 +1,25 @@
-import { Body, Controller, Get, Inject, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Query,
+  Patch,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ItemListServiceInterface } from '../../../application/services/item/item.list.interface';
 import { ItemRegisterServiceInterface } from '../../../application/services/item/item.register.interface';
+import { ItemUpdateServiceInterface } from '../../../application/services/item/item.update.interface';
 import { Observable } from 'rxjs';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ItemListInputDto } from '../../../application/dto/input/item/item.list.input.dto';
 import { ItemListOutputDto } from '../../../application/dto/output/item/item.list.output.dto';
 import { ItemRegisterInputDto } from '../../../application/dto/input/item/item.register.input.dto';
 import { ItemRegisterOutputDto } from '../../../application/dto/output/item/item.register.output.dto';
+import { ItemUpdateInputDto } from '../../../application/dto/input/item/item.update.input.dto';
+import { ItemUpdateOutputDto } from '../../../application/dto/output/item/item.update.output.dto';
 
 @ApiTags('items')
 @Controller('items')
@@ -15,7 +28,9 @@ export class ItemController {
     @Inject('ItemListServiceInterface')
     private readonly ItemListService: ItemListServiceInterface,
     @Inject('ItemRegisterServiceInterface')
-    private readonly ItemRegisterService: ItemRegisterServiceInterface
+    private readonly ItemRegisterService: ItemRegisterServiceInterface,
+    @Inject('ItemUpdateServiceInterface')
+    private readonly ItemUpdateService: ItemUpdateServiceInterface
   ) {}
 
   /**
@@ -57,5 +72,26 @@ export class ItemController {
     @Body() body: ItemRegisterInputDto
   ): Observable<ItemRegisterOutputDto> {
     return this.ItemRegisterService.service(body);
+  }
+
+  /**
+   * @param request - リクエスト情報
+   * @return {Observable<ItemUpdateOutputDto>} - 更新された物品情報を含むObervable
+   */
+  @ApiOperation({
+    summary: '物品を更新するエンドポイント',
+    description: '物品を更新するAPI',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Updated',
+    type: ItemUpdateOutputDto,
+  })
+  @Patch(':item_id')
+  updateItem(
+    @Param('item_id', ParseIntPipe) itemId: number,
+    @Body() body: ItemUpdateInputDto
+  ): Observable<ItemUpdateOutputDto> {
+    return this.ItemUpdateService.service(body, itemId);
   }
 }
