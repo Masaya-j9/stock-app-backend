dc30483aef3c214ef96d8b6df7593a62a5013f2b
Masaya-j9 <masaya.spectre@gmail.com>
2025-04-29 19:30:42 +0900

    add modules about delete

--- src/presentation/modules/items.module.ts
+++ src/presentation/modules/items.module.ts
@@ -4,6 +4,7 @@ import { ItemController } from '../controllers/item/item.controller';
 import { ItemListService } from '../../application/services/item/item.list.service';
 import { ItemRegisterService } from '../../application/services/item/item.register.service';
 import { ItemUpdateService } from '../../application/services/item/item.update.service';
+import { ItemDeleteService } from '../../application/services/item/item.delete.service';
 import { DatabaseModule } from './database.module';
 import { ItemsDatasource } from 'src/infrastructure/datasources/items/items.datasource';
 import { CategoriesModule } from './categories.module';
@@ -24,6 +25,10 @@ import { CategoriesModule } from './categories.module';
     {
       provide: 'ItemUpdateServiceInterface',
       useClass: ItemUpdateService,
+    },
+    {
+      provide: 'ItemDeleteServiceInterface',
+      useClass: ItemDeleteService,
     },
   ],
 })
