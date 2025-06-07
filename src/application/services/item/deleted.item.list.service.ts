395b438e1fcf9ccd2806f45419f791d4751a82e4
Masaya-j9 <masaya.spectre@gmail.com>
2025-06-08 03:58:16 +0900

    add application service

---
+++ src/application/services/item/deleted.item.list.service.ts
@@ -0,0 +1,68 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DeletedItemListServiceInterface } from './deleted.item.list.interface';
+import { Observable, forkJoin, map, of, switchMap, throwError } from 'rxjs';
+import { DeletedItemListInputDto } from '../../dto/input/item/deleted.item.list.input.dto';
+import { DeletedItemListOutputDto } from '../../dto/output/item/deleted.item.list.output.dto';
+import { ItemsDatasource } from '../../../infrastructure/datasources/items/items.datasource';
+import { CategoriesDatasource } from '../../../infrastructure/datasources/categories/categories.datasource';
+import { DeletedItemListOutputBuilder } from '../../dto/output/item/deleted.item.list.output.builder';
+import { ItemDomainFactory } from '../../../domain/inventory/items/factories/item.domain.factory';
+import { CategoryDomainFactory } from '../../../domain/inventory/items/factories/category.domain.factory';
+import { Pagination } from '../../../domain/common/value-objects/pagination';
+import { SortOrder } from '../../../domain/common/value-objects/sort/sort.order';
+
+@Injectable()
+export class DeletedItemListService implements DeletedItemListServiceInterface {
+  // 登録されている物品情報に関するコンストラクタ
+  constructor(
+    public readonly ItemsDatasource: ItemsDatasource,
+    public readonly categoriesDatasource: CategoriesDatasource
+  ) {}
+
+  /**
+   * 論理削除されている物品の一覧を取得する
+   * @param {DeletedItemListInputDto} input - リクエスト情報
+   * @returns {Observable<DeletedItemListOutputDto>} - 論理削除されている物品の一覧情報を含むObservableオブジェクト
+   *
+   * @throws {NotFoundException} 物品が見つからない場合、404エラーをスローします。
+   */
+
+  service(
+    input: DeletedItemListInputDto
+  ): Observable<DeletedItemListOutputDto> {
+    const pagination = Pagination.of(input.pages);
+    const sortOrder = SortOrder.of(input.sortOrder);
+
+    return this.ItemsDatasource.findDeletedItemList(pagination, sortOrder).pipe(
+      switchMap((deletedItems) => {
+        if (deletedItems.length === 0) {
+          return throwError(() => new NotFoundException('Items not found'));
+        }
+        const itemIds = deletedItems.map((item) => item.id);
+        return forkJoin([
+          this.categoriesDatasource.findByCategories(itemIds),
+          this.categoriesDatasource.findCategoryIdsAndItemIds(itemIds),
+          of(deletedItems),
+          this.ItemsDatasource.countDeletedAll(),
+        ]);
+      }),
+      map(
+        ([categories, itemIdsAndCategoryIds, deletedItems, totalItemCount]) => {
+          const totalCount = deletedItems.length;
+          const deletedDomainItems = deletedItems.map((item) =>
+            ItemDomainFactory.fromInfrastructure(item, itemIdsAndCategoryIds)
+          );
+          const domainCategories = categories.map((category) =>
+            CategoryDomainFactory.fromInfrastructure(category)
+          );
+          return new DeletedItemListOutputBuilder(
+            deletedDomainItems,
+            totalCount,
+            pagination.calcTotalPages(totalItemCount),
+            domainCategories
+          ).build();
+        }
+      )
+    );
+  }
+}
