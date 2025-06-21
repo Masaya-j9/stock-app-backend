fb0eeac20d0b8af3c71672a350b803e2a030fb9e
Masaya-j9 <masaya.spectre@gmail.com>
2025-06-22 01:40:56 +0900

    add I/O

---
+++ src/application/dto/input/item/deleted.item.single.input.dto.ts
@@ -0,0 +1,17 @@
+import { InputDto } from '../input.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsInt, Min } from 'class-validator';
+
+export class DeletedItemSingleInputDto implements InputDto {
+  @ApiProperty({
+    example: '1',
+    description: '物品ID',
+    type: Number,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  @Transform(({ value }) => parseInt(value))
+  id: number;
+}
