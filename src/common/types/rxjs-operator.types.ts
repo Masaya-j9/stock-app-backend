b74bfc8657545628658dee2b65458ed546da57d2
Masaya-j9 <masaya.spectre@gmail.com>
2025-06-22 23:41:41 +0900

    add operator types

--- src/common/types/rxjs-operator.types.ts
+++ src/common/types/rxjs-operator.types.ts
@@ -1,10 +1,36 @@
 import { Items } from '../../infrastructure/orm/entities/items.entity';
 import { Categories } from '../../infrastructure/orm/entities/categories.entity';
+import { Category } from '../../domain/inventory/items/entities/category.entity';
 import { OperatorFunction } from 'rxjs';
 
 export type ItemNotFoundOperator = OperatorFunction<Items | undefined, Items>;
 
+export type ItemListNotFoundOperator = OperatorFunction<
+  Items[] | undefined,
+  Items[]
+>;
+
+export type CategoryNotFoundOperator = OperatorFunction<
+  Categories | undefined,
+  Categories
+>;
+
 export type CategoriesNotFoundOperator = OperatorFunction<
   Categories[] | undefined,
   Categories[]
+>;
+
+export type CategoryIdsNotFoundOperator = OperatorFunction<
+  number[] | undefined,
+  number[]
+>;
+
+export type ItemNameUniqueOperator = OperatorFunction<
+  Items | undefined,
+  Items | undefined
+>;
+
+export type CategoryUpdateConflictOperator = OperatorFunction<
+  Category | null | undefined,
+  Category
 >;
