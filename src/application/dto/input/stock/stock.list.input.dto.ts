4d0c02531ced184a0e28d3a8bedc737b81d9b425
Masaya-j9 <masaya.spectre@gmail.com>
2025-07-13 16:15:33 +0900

    add I/O

--- src/application/dto/input/stock/stock.list.input.dto.ts
+++ src/application/dto/input/stock/stock.list.input.dto.ts
@@ -1,3 +1,42 @@
 import { InputDto } from '../input.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, Min, Max } from 'class-validator';
+import { Expose, Transform } from 'class-transformer';
 
-export class StockListInputDto implements InputDto {}
+/*
+ * 在庫一覧を取得する際のリクエスト情報を表すDTO
+ */
+const PAGE_MIN: number = 1;
+const SORT_ORDER_MIN: number = 0;
+const SORT_ORDER_MAX: number = 1;
+
+export class StockListInputDto implements InputDto {
+  @ApiProperty({
+    example: 1,
+    description: `
+    ページ番号(1以上の整数とする)
+    1ページあたりの取得件数は10件とする
+    `,
+    type: Number,
+  })
+  @IsOptional()
+  @Min(PAGE_MIN)
+  @Expose({ name: 'pages' })
+  pages?: number;
+
+  @ApiProperty({
+    example: 0,
+    description: `
+    並べ替え順
+    0: 昇順(省略された場合はデフォルトとする)
+    1: 降順
+    `,
+    type: Number,
+  })
+  @IsOptional()
+  @Min(SORT_ORDER_MIN)
+  @Max(SORT_ORDER_MAX)
+  @Transform(({ value }) => Number(value))
+  @Expose({ name: 'sort_order' })
+  sortOrder?: number;
+}
--- src/application/dto/output/stock/stock.list.output.dto.ts
+++ src/application/dto/output/stock/stock.list.output.dto.ts
@@ -1,3 +1,126 @@
-import { OutputDto } from '../output.dto';
+import { OutputDto } from '../../output/output.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
 
-export class StockListOutputDto implements OutputDto {}
+export class StockItem {
+  @ApiProperty({
+    example: 1,
+    description: '物品ID',
+    type: Number,
+  })
+  @Expose({ name: 'id' })
+  id: number;
+
+  @ApiProperty({
+    example: 'テスト物品',
+    description: '物品名',
+    type: String,
+  })
+  @Expose({ name: 'name' })
+  name: string;
+}
+
+export class StockResults {
+  @ApiProperty({
+    example: 1,
+    description: '在庫ID',
+    type: Number,
+  })
+  @Expose({ name: 'id' })
+  id: number;
+
+  @ApiProperty({
+    example: 10,
+    description: '在庫数量',
+    type: Number,
+  })
+  @Expose({ name: 'quantity' })
+  quantity: number;
+
+  @ApiProperty({
+    example: 'この在庫はテスト用です。',
+    description: '在庫の説明',
+    type: String,
+  })
+  @Expose({ name: 'description' })
+  description: string;
+
+  @ApiProperty({
+    example: '2021-01-01T00:00:00.000Z',
+    description: '作成日時',
+    type: Date,
+  })
+  @Expose({ name: 'created_at' })
+  createdAt: Date;
+
+  @ApiProperty({
+    example: '2021-01-01T00:00:00.000Z',
+    description: '更新日時',
+    type: Date,
+  })
+  @Expose({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Type(() => StockItem)
+  @ApiProperty({
+    example: {
+      id: 1,
+      name: 'テスト物品',
+    },
+    description: '関連する物品情報',
+    type: StockItem,
+    nullable: true,
+  })
+  @Expose({ name: 'item' })
+  item: StockItem | null;
+}
+
+export class StockListOutputDto implements OutputDto {
+  @ApiProperty({
+    example: 1,
+    description: '1ページあたりの在庫数',
+    type: Number,
+  })
+  @Expose({ name: 'count' })
+  count: number;
+
+  @ApiProperty({
+    example: 6,
+    description: '総ページ数',
+    type: Number,
+  })
+  @Expose({ name: 'total_pages' })
+  totalPages: number;
+
+  @Type(() => StockResults)
+  @ApiProperty({
+    example: [
+      {
+        id: 1,
+        quantity: 10,
+        description: 'この在庫はテスト用です。',
+        createdAt: '2021-01-01T00:00:00.000Z',
+        updatedAt: '2021-01-01T00:00:00.000Z',
+        item: {
+          id: 1,
+          name: 'テスト物品',
+        },
+      },
+      {
+        id: 2,
+        quantity: 20,
+        description: 'この在庫はテスト用です。',
+        createdAt: '2021-01-01T00:00:00.000Z',
+        updatedAt: '2021-01-01T00:00:00.000Z',
+        item: {
+          id: 2,
+          name: 'テスト物品2',
+        },
+      },
+    ],
+    description: '在庫一覧（物品情報含む）',
+    type: [StockResults],
+  })
+  @Expose({ name: 'results' })
+  results: StockResults[];
+}
