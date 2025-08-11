5bba10b0fa1761ff8af0d86520d97cd1cfa51aad
Masaya-j9 <masaya.spectre@gmail.com>
2025-08-12 01:55:23 +0900

    add seed data list

---
+++ src/infrastructure/orm/seeds/seed.data.config.ts
@@ -0,0 +1,20 @@
+/**
+ * シードデータ配列の命名パターン設定
+ * ファクトリーファイル内でエクスポートされているマスターデータ配列名のパターンを定義
+ */
+export const SEED_DATA_PATTERNS = [
+  'stockHistoryStatusData',
+  'categoryData',
+  'roleData',
+  'statusData',
+  'returnsData',
+  'profileData',
+  'stockData',
+
+  // intermediatesディレクトリのマスターデータ変数名
+  'borrowingCommensData',
+  'borrowingReturnsData',
+  'borrowingStocksData',
+  'returnStocksData',
+  'userBorrowingsData',
+];
