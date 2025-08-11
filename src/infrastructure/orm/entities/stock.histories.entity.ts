3c7145e3a356476f321ade880a51f6abe72a435e
Masaya-j9 <masaya.spectre@gmail.com>
2025-08-11 22:17:54 +0900

    add foreign keys

--- src/infrastructure/orm/entities/stock.histories.entity.ts
+++ src/infrastructure/orm/entities/stock.histories.entity.ts
@@ -7,6 +7,7 @@ import {
   DeleteDateColumn,
 } from 'typeorm';
 import { Stocks } from './stocks.entity';
+import { StockHistoryStatuses } from './stock.history.statuses.entity';
 
 /**
  * 在庫の履歴を管理するStockHistoriesテーブルのエンティティ
@@ -20,6 +21,11 @@ export class StockHistories {
   @ManyToOne(() => Stocks, (stock) => stock.stockHistories)
   @JoinColumn({ name: 'stock_id' })
   stock: Stocks;
+
+  //statusはStockHistoryStatusesエンティティを参照する
+  @ManyToOne(() => StockHistoryStatuses, (status) => status.stockHistories)
+  @JoinColumn({ name: 'stock_history_status_id' })
+  status: StockHistoryStatuses;
 
   @Column()
   quantity: number;
